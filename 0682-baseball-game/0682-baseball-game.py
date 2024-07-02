0414d5688d8cb8eb7509e1575ba8d57330a0e453
Maryam Sikander <119351721+Maryam-Sikander@users.noreply.github.com>
2024-07-02 00:56:10 -0700

    Time: 43 ms (61.96%), Space: 16.6 MB (97.92%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -2,17 +2,51 @@ class Solution:
     def calPoints(self, operations: List[str]) -> int:
         stack = []
 
-        for ele in operations:
-            if ele.isdigit() or (ele[0] == '-' and ele[1:].isdigit()):
-                stack.append(int(ele))
-            elif ele == 'C':
-               if stack:
-                stack.pop()
-            elif ele == 'D':
+        for op in operations:
+            if op.isdigit() or (op[0] == '-' and op[1:].isdigit()):
+                stack.append(int(op)) 
+            
+            elif op == "D":
                 if stack:
                     stack.append(stack[-1] * 2)
-            elif ele == "+":
+            elif op == "C":
+                if stack:
+                    stack.pop()
+            elif op == "+":
                 if len(stack) >= 2:
                     stack.append(stack[-1] + stack[-2])
-                    
+            
         return sum(stack)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # stack = []
+        # for ele in operations:
+        #     if ele.isdigit() or (ele[0] == '-' and ele[1:].isdigit()):
+        #         stack.append(int(ele))
+        #     elif ele == 'C':
+        #        if stack:
+        #         stack.pop()
+        #     elif ele == 'D':
+        #         if stack:
+        #             stack.append(stack[-1] * 2)
+        #     elif ele == "+":
+        #         if len(stack) >= 2:
+        #             stack.append(stack[-1] + stack[-2])
+                    
+        # return sum(stack)
