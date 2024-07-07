55d8a585077b8c5735820d75e2c7cf2181b35a43
Maryam Sikander <119351721+Maryam-Sikander@users.noreply.github.com>
2024-07-07 00:41:10 -0700

    Time: 42 ms (79.85%), Space: 16.6 MB (10.63%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,21 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        total = 0
+        s = s.replace('IV', 'IIII')
+        s = s.replace('IX', 'VIIII')
+        s = s.replace('XL', 'XXXX')
+        s = s.replace('XC', 'LXXXX')
+        s = s.replace('CD', 'CCCC')
+        s = s.replace('CM', 'DCCCC')
+        for i in s:
+            total += roman[i]
+        return total
